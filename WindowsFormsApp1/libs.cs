0c3d4c98bc4e500c2aed41fc27679d9bf809f226
bacloud14 <bacloud14@gmail.com>
2022-09-18 00:08:06 +0200

    .

--- WindowsFormsApp1/libs.cs
+++ WindowsFormsApp1/libs.cs
@@ -24,5 +24,33 @@ namespace WindowsFormsApp1
 
             return bmp;
         }
+
+        public static Bitmap TakeScreenshot2(string filePath = null)
+        {
+            var rect = new Rectangle();
+
+            // Take a screenshot that includes all screens by
+            // creating a rectangle that captures all monitors
+            foreach (var screen in Screen.AllScreens)
+            {
+                var bounds = screen.Bounds;
+                var width = bounds.X + bounds.Width;
+                var height = bounds.Y + bounds.Height;
+
+                if (width > rect.Width) rect.Width = width;
+                if (height > rect.Height) rect.Height = height;
+            }
+
+            var bmp = new Bitmap(rect.Width, rect.Height);
+
+            using (var g = Graphics.FromImage(bmp))
+            {
+                g.CopyFromScreen(0, 0, 0, 0, rect.Size);
+            }
+
+            if (filePath != null) bmp.Save(filePath);
+
+            return bmp;
+        }
     }
 }
