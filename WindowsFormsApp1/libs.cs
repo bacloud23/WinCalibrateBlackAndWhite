dcab4f8d2b62bb84d83df5b1c9a8abdbf7006e33
bacloud14 <bacloud14@gmail.com>
2022-09-18 00:04:39 +0200

    Add project files.

---
+++ WindowsFormsApp1/libs.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    internal class libs
+    {
+        public static Bitmap TakeScreenshot(string filePath = null)
+        {
+            var bounds = Screen.PrimaryScreen.Bounds;
+            var bmp = new Bitmap(bounds.Width, bounds.Height);
+
+            using (var g = Graphics.FromImage(bmp))
+            {
+                g.CopyFromScreen(0, 0, 0, 0, bounds.Size);
+            }
+
+            if (filePath != null) bmp.Save(filePath);
+
+            return bmp;
+        }
+    }
+}
