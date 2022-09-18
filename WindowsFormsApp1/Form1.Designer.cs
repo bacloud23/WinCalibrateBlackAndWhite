4e817111247ca3eb448f093fb2f00f95fb34b19e
bacloud14 <bacloud14@gmail.com>
2022-09-18 17:55:18 +0200

    gut

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -1,4 +1,6 @@
-﻿namespace WindowsFormsApp1
+﻿using System.Windows.Forms;
+
+namespace WindowsFormsApp1
 {
     partial class Form1
     {
@@ -29,11 +31,14 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.TextBox2 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(330, 133);
+            this.button1.Location = new System.Drawing.Point(58, 23);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
@@ -41,22 +46,57 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(58, 53);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(75, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(165, 23);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(342, 76);
+            this.panel1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.TextBox2.Location = new System.Drawing.Point(58, 79);
+            this.TextBox2.Name = "textBox2";
+            this.TextBox2.Size = new System.Drawing.Size(75, 20);
+            this.TextBox2.TabIndex = 3;
+            this.TextBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(529, 142);
+            this.Controls.Add(this.TextBox2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private Panel panel1;
+        private TextBox textBox2;
+
+        public TextBox TextBox1 { get => textBox1; set => textBox1 = value; }
+        public TextBox TextBox2 { get => textBox2; set => textBox2 = value; }
+
+        public Panel Panel1 { get => panel1; set => panel1 = value; }
+        
     }
 }
-
--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -26,8 +26,27 @@ namespace WindowsFormsApp1
         {
             Console.WriteLine("Taking a screenshot now...");
             Bitmap bmp = libs.TakeScreenshot("screenshot.bmp");
-            libs.GetAverageColorInRegion(bmp);
+            uint[] color = libs.GetAverageColorInRegion(bmp);
+
+            string ss = string.Join(" ", color.Select(x => x.ToString()).ToArray());
+            TextBox1.Text = ss;
+            Color target = new Color();
+            target = Color.FromArgb((int)color[0], (int)color[1], (int)color[2]);
+            Panel1.BackColor = target;
+            textBox2.Text = target.GetBrightness().ToString();
+
             Console.WriteLine("\nDone! Press any key to exit...");
+            
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
